d1e239b474eedf8dbd2d26a70426d9ddd37f21dc
MerkurevSergei <MerkurevSergei@yandex.ru>
2017-08-20 17:56:32 +0900

    Тест проекта SecretAlbum

---
+++ source/assets/other/projects/SecretAlbum/js/app.js
@@ -0,0 +1,11 @@
+;(function() {
+    'use strict';
+    var content = document.querySelector(".content");
+    var strings = document.querySelectorAll(".content__text");
+    
+    for(var i=0; i<strings.length; ++i) 
+    {
+        var string = strings[i];
+        //alert(string.clientWidth);
+    }
+})();
