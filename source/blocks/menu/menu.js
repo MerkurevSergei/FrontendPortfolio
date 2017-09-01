1012339964853be7a2a2f147d56aeabe4e805b4f
Merkurev Sergei <MerkurevSergei@yandex.ru>
2017-09-01 14:24:27 +0900

    Доработка функционала блока меню

--- source/blocks/menu/menu.js
+++ source/blocks/menu/menu.js
@@ -1,6 +1,5 @@
 ;(function () {
     var sandwich = document.querySelector(".sandwich");
-    var menuList = document.querySelector(".menu__links");
     var menuLinks = document.querySelectorAll(".menu__link");
     
     //var closeWriteForm = document.querySelector(".write-form__close");
@@ -8,14 +7,18 @@
     // Open/Close menu on Sandwich
     sandwich.addEventListener("click", function (event) {
         event.preventDefault();
-        menuList.classList.toggle("display-off");
+        for (var i = 0; i < menuLinks.length; i++) {
+            menuLinks[i].classList.toggle("display-off");
+        }
     });
 
     // Close menu on Link
     for (var i = 0; i < menuLinks.length; i++) {
         var menuLink = menuLinks[i];
         menuLink.addEventListener("click", function (event) {
-            menuList.classList.add("display-off");
+            for (var i = 0; i < menuLinks.length; i++) {
+                menuLinks[i].classList.add("display-off");
+            }
         });
     }
 })();
